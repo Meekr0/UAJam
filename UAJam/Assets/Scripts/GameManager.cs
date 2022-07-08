8013cccc676becc3623540aa9a8bea5b3533aadd
Jan Michalski <60537290+Meekr0@users.noreply.github.com>
2022-07-09 00:38:08 +0200

    prototype level, debug tile building

--- UAJam/Assets/Scripts/GameManager.cs
+++ UAJam/Assets/Scripts/GameManager.cs
@@ -1,21 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GameManager : MonoBehaviour
 {
     private bool isInSpiritWorld = false;
-    
+
+    [SerializeField] Tilemap baseTileMap;
+    [SerializeField] Tilemap pathTileMap;
+    [SerializeField] Tilemap hidePathTileMap;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        baseTileMap.gameObject.SetActive(true);
+        pathTileMap.gameObject.SetActive(false);
+        hidePathTileMap.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Debug.Log(isInSpiritWorld);
+        if(isInSpiritWorld)
+        {
+            pathTileMap.gameObject.SetActive(true);
+            hidePathTileMap.gameObject.SetActive(false);
+        }
+        else
+        {
+            pathTileMap.gameObject.SetActive(false);
+            hidePathTileMap.gameObject.SetActive(true);
+        }
     }
 
     public void changeSpiritWorld(bool val)
