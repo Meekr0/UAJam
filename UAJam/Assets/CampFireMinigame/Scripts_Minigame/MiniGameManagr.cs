c2893976a13035ac0acfec91541aa7242e66c04d
wtrzeci <72412401+wtrzeci@users.noreply.github.com>
2022-07-09 22:27:57 +0200

    Skończona Minigiera ognisko-gameloop

--- UAJam/Assets/CampFireMinigame/Scripts_Minigame/MiniGameManagr.cs
+++ UAJam/Assets/CampFireMinigame/Scripts_Minigame/MiniGameManagr.cs
@@ -2,15 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MiniGameManagr : MonoBehaviour
 {
+    public bool started = false;
+    bool end = true;
     [SerializeField] GameObject ScoreField;
     [SerializeField] private GameObject TimeField;
     [SerializeField] private float TimeLimit = 60;
     [SerializeField] private int ScoreToWin = 20;
     [SerializeField] private GameObject tutorial;
+    [SerializeField] private GameObject outro;
     public int Score = 0;
    public static MiniGameManagr Instance;
     // Start is called before the first frame update
@@ -37,16 +41,24 @@ public class MiniGameManagr : MonoBehaviour
 
     public void Win()
     {
-        if (Score >= ScoreToWin)
+        if (Score >= ScoreToWin )
         {
             Debug.Log("Win");
+            Score = ScoreToWin;
+            Time.timeScale = 0;
+            outro.gameObject.SetActive(true);
+            end = !end;
+            if (Input.anyKey & end)
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
         }
     }
 
     private void ResumeGame()
     {
-        if (Input.GetKey(KeyCode.Backspace))
+        if (Input.GetKey(KeyCode.Space))
         {
+            started = true;
             Time.timeScale = 1f;
             Destroy(tutorial.gameObject);
         }
