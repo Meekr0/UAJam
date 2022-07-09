b8764871cc2b30b67790acc3892c297c5765d0ca
Meekr0 <janmichalski@hotmail.com>
2022-07-10 01:12:33 +0200

    audio manager + hej sokoly.wav

---
+++ UAJam/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private void Awake()
+    {
+
+        SetUpSingleton();
+
+    }
+
+    private void SetUpSingleton()
+    {
+
+        if(FindObjectsOfType(GetType()).Length > 1)
+        {
+
+            Destroy(gameObject);
+
+        } 
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+}
