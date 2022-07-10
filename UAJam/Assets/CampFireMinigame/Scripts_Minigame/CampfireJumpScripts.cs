8ec5de789c66e876cf1a3aac1c821951df0ea9a0
wtrzeci <72412401+wtrzeci@users.noreply.github.com>
2022-07-10 02:02:19 +0200

    Naprawienie MinigryOgniska

--- UAJam/Assets/CampFireMinigame/Scripts_Minigame/CampfireJumpScripts.cs
+++ UAJam/Assets/CampFireMinigame/Scripts_Minigame/CampfireJumpScripts.cs
@@ -8,6 +8,9 @@ using UnityEngine.UIElements;
 
 public class CampfireJumpScripts : MonoBehaviour
 {
+    [SerializeField] private float _JumpCD = 0.01f;
+    [SerializeField]private float JumpCD = 0;
+    [SerializeField]private float JumpTimer = 0;
     private Rigidbody2D thisRigidbody2D;
     private int JumpIndex = 0;
     private float startHeight;
@@ -28,8 +31,9 @@ public class CampfireJumpScripts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        JumpTimer += Time.deltaTime;
         if (thisRigidbody2D.velocity.x < 0.001f & thisRigidbody2D.velocity.y < 0.001f &
-            math.abs((this.transform.position.y - startHeight)) < 0.1f)
+            math.abs((this.transform.position.y - startHeight)) < 0.001f)
         {
             if (grounded)
             {
@@ -40,13 +44,14 @@ public class CampfireJumpScripts : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) & MiniGameManagr.Instance.started )
         {
             Jumping();
+            JumpCD = JumpTimer + _JumpCD;
         }
 
     }
 
     void Jumping()
     {
-        if (thisRigidbody2D.velocity.x < 0.001f & thisRigidbody2D.velocity.y < 0.001f & math.abs((this.transform.position.y - startHeight))<0.1f)
+        if (thisRigidbody2D.velocity.x < 0.1f & thisRigidbody2D.velocity.y < 0.1f & math.abs((this.transform.position.y - startHeight))<0.1f & JumpTimer>=JumpCD )
         {
             if (JumpIndex % 2 == 0)
                 thisRigidbody2D.AddForce(transform.up * JumpingModifier + transform.right * JumpingModifier);
@@ -61,6 +66,11 @@ public class CampfireJumpScripts : MonoBehaviour
             this.GetComponent<Animator>().enabled = true;
 
         }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        grounded = true;
     }
 
     private void DisableAnimator()
--- UAJam/Assets/RiverMinigame/Scripts/WrethGameManager.cs
+++ UAJam/Assets/RiverMinigame/Scripts/WrethGameManager.cs
@@ -2,13 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class WrethGameManager : MonoBehaviour
 {
+    [SerializeField] private string GameLostString;
+    [SerializeField] private string GameWinString;
+    [SerializeField] private GameObject Clock;
     [SerializeField] private GameObject TextBG;
     [SerializeField] private GameObject Text;
     [SerializeField] private float TimeToSurvive = 30f;
     static public WrethGameManager Instance;
+    [SerializeField] private string EntryText;
+    private bool lost = false;
+    private bool staring = true;
 
     private float Timer;
     // Start is called before the first frame update
@@ -21,28 +29,60 @@ public class WrethGameManager : MonoBehaviour
     void Start()
     {
         Time.timeScale = 0;
-        
+        Text.SetActive(true);
+        TextBG.SetActive(true);
+        Text.GetComponent<Text>().text = EntryText;
+
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (staring & Input.GetKey(KeyCode.Space))
+        {
+            Time.timeScale = 1;
+            Text.SetActive(false);
+            TextBG.SetActive(false);
+            staring = false;
+        }
         Timer -= Time.deltaTime;
         if (Timer <= 0)
         {
-            Debug.Log("Win");
+            EndGameWin();
+            Timer = 0;
         }
-
+        int rounded = Convert.ToInt32(TimeToSurvive);
+        TimeToSurvive -= Time.deltaTime;
+        Clock.GetComponent<Text>().text = rounded.ToString();
+        if (Input.GetKey(KeyCode.Space) & lost)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex );
+        }
     }
 
     public void EndGameWin()
     {
         Debug.Log("Win");
+        Time.timeScale = 0;
+        Text.SetActive(true);
+        TextBG.SetActive(true);
+        Text.GetComponent<Text>().text = GameWinString;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 
     public void EndGameLose()
     {
+        Debug.Log("Lost");
+        Time.timeScale = 0;
+        lost = true;
+        Text.SetActive(true);
+        TextBG.SetActive(true);
+        Text.GetComponent<Text>().text = GameLostString;
         Debug.Log("Lost");
     }
     
